f452c9c20348cd9ee7a83898485fdf07488a1d22
Sahith Bodla <sahith.bodla@gmail.com>
2022-03-18 09:07:14 +0530

    Intro

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const message = React.createElement('p',{},'I am Learning React');
+// const header = React.createElement('h2',{},"React Basics");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const flexBox = React.createElement('div',{},React.createElement('div',{},React.createElement('h1',{},"Hello World")));
+
+// // shorthand
+// const e = React.createElement;
+// const flexBox2 = e('div',{},e('div',{},e('h1',{},"Hello World")));
+
+// JSX - Javascript XML
+const name = "Toffi";
+
+// Style Object
+const colorObject = {
+  color: 'red',
+  margin: '10rem',
+  fontSize: '4rem',
+  backgroundColor: "blue"
+};
+
+// Accessing Javascript Variable inside JSX - we need to use expression
+const firstJSX = (
+  <>
+    <h1 className="mystyle">Hello {name}</h1>
+  </>
+)
+
+ReactDOM.render(firstJSX,document.getElementById('root'));
