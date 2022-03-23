4ac11003105457162e816e0ea02e3fabb6ecf39e
Sahith Bodla <sahith.bodla@gmail.com>
2022-03-23 09:35:35 +0530

    Tic Tac Toe Starter Code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
+import Game from './game/Game';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<Game/>,document.getElementById('root'));
 
 
 
